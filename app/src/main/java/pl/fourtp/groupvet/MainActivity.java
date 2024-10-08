28fcf2c0bb11ea4171700791bf84c8209b4da7a0
kondziux1 <konrad.frukacz@op.pl>
2024-10-08 10:54:14 +0200

    Suwak działa i moj sprzęt !

--- app/src/main/java/pl/fourtp/groupvet/MainActivity.java
+++ app/src/main/java/pl/fourtp/groupvet/MainActivity.java
@@ -1,12 +1,19 @@
 package pl.fourtp.groupvet;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +27,40 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        View V = getCurrentFocus();
+        ListView listView = findViewById(R.id.listviu);
+
+        ArrayList<String> gatunki = new ArrayList<String>();
+        gatunki.add("Pies");
+        gatunki.add("Kot");
+        gatunki.add("Świnka Morska");
+
+        ArrayAdapter<String> arradapt = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,gatunki);
+        listView.setAdapter(arradapt);
+        listView.setSelector(R.color.mil);
+
+        SeekBar suwak = findViewById(R.id.suwak);
+        TextView suwakinfo = findViewById(R.id.suwakinfo);
+
+        suwak.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
+                suwakinfo.setText("Ile ma lat? " + progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
+
+
 }
