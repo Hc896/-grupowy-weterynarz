207741c4792efb71cd945d1ad177fc65fa815af4
DESKTOP-O3QOBC5\DariuszSztamb4TP <makasz1977@gmail.com>
2024-10-15 10:56:07 +0200

    Dariusz

---
+++ DokumentacjaAplikacji.py
@@ -0,0 +1,13 @@
+#**********************************************************************************************************
+# Klasa: Vowos
+# opis: Klasa ma charakter biblioteki narzędziowej, nie jest przewidziane powoływanie obiektów tej klasy, 
+# wszystkie jej metody są statyczne i są widzialne poza klasą
+#
+# Klasa zawiera dwie metody:
+#   - Metoda CountVowos - liczy samogłoski
+#   - Metoda DeleteDuplicate - Wyświetla wprowadzony tekst bez powtórzeń
+# metody: CountVowos - Zwraca liczbę samogłosek znajdujących się w parametrze.
+#         W przypadku, gdy paratrem jest pustym łańcuchem znaków lub null metoda zwraca 0 
+#         DeleteDuplicate - zwraca łańcuch znaków bez powtórzeń
+# autor: 000000000000
+#**********************************************************************************************************
